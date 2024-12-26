fa3d61544fdc6726c82ad5ef1b1318f5ac082c5c
msvgeek <matt@vgeek.guru>
2024-12-26 10:39:49 -0500

    msvgeek

--- src/navigation.ts
+++ src/navigation.ts
@@ -9,6 +9,32 @@ export const headerData = {
     {
       text: 'Services',
       href: getPermalink('/services'),
+      links: [
+        {
+          text: 'Cyber Security',
+          href: getPermalink('/cyber-security'),
+        },
+        {
+          text: 'Strategic Planning',
+          href: getPermalink('/strategic-planning'),
+        },
+        {
+          text: 'Managed Services',
+          href: getPermalink('/managed-services'),
+        },
+        {
+          text: 'IT Support',
+          href: getPermalink('/it-support'),
+        },
+        {
+          text: 'Data Protection',
+          href: getPermalink('/data-protection'),
+        },
+        {
+          text: 'Co-Managed IT',
+          href: getPermalink('/comanaged-it'),
+        }
+      ],
     },
     {
       text: 'Pricing',
